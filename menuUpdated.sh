99ad090b5c5e54a176d87b3bb2a08e96a9a01116
Shachar Shapira <shapi78@gmail.com>
2022-05-29 19:02:42 +0300

    Fixing menu stracture

--- menuUpdated.sh
+++ menuUpdated.sh
@@ -1,17 +1,7 @@
 #! /usr/bin/env bash
 # IP config menu script
 
-####################
-#([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])
-
-# I want 1-254
-# can spread 
-# 1-99    [1-9][0-9]
-# 100-199 1[0-9]{2}
-# 200-249 2[0-4]00-9]
-# 250-254 25[0-4]
-# [1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4]
-#((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+selectedIf="eth0"
 
 AskForSubnetMask ()
 
@@ -36,7 +26,7 @@ AskForSubnetMask ()
 }
 
 
-selectIP ()
+configureIP ()
 {
                while true    
                do
@@ -44,7 +34,7 @@ selectIP ()
                     read -p 'Enter IP: ' IP_STRING
                     if [[ $IP_STRING =~ ^$rx ]]; then
                          echo "success"
-                         read -p "$IP_STRING are you sure? (y =yes)"  CHECK
+                         read -p "$IP_STRING are you sure? (y = yes)"  CHECK
                          if [[ "$CHECK" == "y" ]]; then
                               break
                          fi
@@ -56,7 +46,7 @@ selectIP ()
 
 }
 
-selectGateway ()
+configureGateway ()
 {
         while true    
                     do
@@ -64,7 +54,7 @@ selectGateway ()
                               read -p 'Enter Default Gateway: ' DEFAULT_GATEWAY
                               if [[ $DEFAULT_GATEWAY =~ ^$rx ]]; then
                                    echo "success"
-                                   read -p "$DEFAULT_GATEWAY are you sure? (y =yes)"  CHECK
+                                   read -p "$DEFAULT_GATEWAY are you sure? (y = yes)"  CHECK
                                    if [[ "$CHECK" == "y" ]]; then
                                         break
                                    fi
@@ -80,7 +70,7 @@ selectGateway ()
 
 }
 
-selectNetwork()
+configureNetwork()
 {
 
 
@@ -104,9 +94,9 @@ selectNetwork()
 
 editSNMP()
 {
-          apt update
-                         apt install snmpd
-                         apt-get install snmpd snmp snmp-mibs-downloader          
+          		 # apt update
+                         # apt install snmpd
+                         # apt-get install snmpd snmp snmp-mibs-downloader          
                          snmpconf
                               
                               
@@ -126,7 +116,7 @@ editSNMP()
                               net-snmp-config --create-snmpv3-user -ro
                          fi  
 }
-selectDNS()
+configureDNS()
 {
      while true
                          do
@@ -154,21 +144,21 @@ configMenu () {
                do
                case $opt in
                     "Config IP")
-                         selectIP
+                         configureIP
                          ;;
                     "Configure SNMP")
                          editSNMP
                          ;;
                     "Configure IP Route")
-                         selectNetwork
-                         selectGateway
+                         configureNetwork
+                         configureGateway
                          echo "  - gw: $DEFAULT_GATEWAY" >> routes.yaml
                          ;;
                     "Configure DNS")
-                         selectDNS
+                         configureDNS
                          ;;
                     "Configure Default Gateway")
-                         selectGateway
+                         configureGateway
                          ;;
                     "Quit")
                          break
@@ -177,6 +167,18 @@ configMenu () {
                     esac
      done
 }
+
+selectInterface() {
+	local PS3="Which interface do u want to config? "
+	countInterfaces=$(nmcli device status | awk '/ethernet/{print $1}')
+	select interface in ${countInterfaces[@]}
+	do  
+     		echo "Selected interface: $interface"
+     		interfaceName=$interface
+     		break 
+	done
+}
+
 displayMenu()
 {
              local PS3='Please enter sub option: '
@@ -224,20 +226,10 @@ CIDR=$(cut -d "/" -f2 <<< $(ip addr show | grep -v 'forever\|valid\|link\|::\|lo
 echo "Cervello Configurator"
 
 # countInterfaces=$(ip addr show | grep -v 'forever\|valid\|link\|::\|lo:\|host\|NO\|/' | awk '{print $2}' | sed -n '$=')
-countInterfaces=$(nmcli device status | awk '/ethernet/{print $1}')
-PS3="Which interface do u want to config?"
-select interface in ${countInterfaces[@]}
-do  
-     echo "Selected number: $interface"
-     interfaceName=$interface
-     break 
-done
 ########################
-echo -e "\n  - name: $interfaceName" >> interfaces.yaml
+# echo -e "\n  - name: $interfaceName" >> interfaces.yaml
 #############################      
 
-#!/bin/bash
-# Bash Menu Script Example
 
 PS3='Please enter your choice: '
 options=("Config Parameters" "Display Parameters" "Quit")
@@ -245,6 +237,7 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Config Parameters")
+	      selectInterface
               configMenu
           ;;
           "Display Parameters")   
